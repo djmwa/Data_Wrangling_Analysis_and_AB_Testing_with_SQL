ff8c99dee2541994d7ca99cecf741111b6d0c7ed
djmwa <djmoser.wa@gmail.com>
2025-03-12 15:22:45 -0700

    Update Exercise 1.5.sql

--- Exercise 1.5.sql
+++ Exercise 1.5.sql
@@ -1,7 +1,7 @@
 --[Mode](https://app.mode.com/) is the only data source used for this project.
 
 --Goal: Count and return the net users added each day, accounting for new, deleted, and merged users
---Report with visualization of the data is provided in 'Exercise 1.5.pdf' in the same folder this file is in
+--Report with visualization of the query results is provided in 'Exercise 1.5.pdf' in the same folder this file is in
 
 --Step 1: Get a total user count
 SELECT 
