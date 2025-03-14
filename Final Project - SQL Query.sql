c05e022874398ddb258a6d0920226f8a66a496f4
djmwa <djmoser.wa@gmail.com>
2025-03-14 00:05:53 -0700

    Update Final Project - SQL Query.sql

--- Final Project - SQL Query.sql
+++ Final Project - SQL Query.sql
@@ -1,5 +1,3 @@
---[Mode](https://app.mode.com/) is the only data source used for this project.
-
 --We are running an experiment at an item-level, which means all users who visit will see the same page, but the layout of different item pages may differ.
 --Compare this table to the assignment events we captured for user_level_testing.
 --Does this table have everything you need to compute metrics like 30-day view-binary?
